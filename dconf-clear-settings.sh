901e57b4481a5b15320e61e4a375ed48119f2c8e
ttyS3 <ttys3.rust@gmail.com>
2024-12-04 15:10:08 +0000

    feat: add scripts for managing dconf settings

---
+++ dconf-clear-settings.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -eou pipefail
+
+dconf reset -f /org/gnome/shell/extensions/cursor-usage/
+
+
---
+++ dconf-list.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+set -eou pipefail
+
+dconf list /org/gnome/shell/extensions/cursor-usage/
+
